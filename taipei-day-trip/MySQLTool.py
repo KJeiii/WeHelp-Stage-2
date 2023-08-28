8640b3142e430ac24abd5d9b406a80a0d5bcf4ab
KJeiii <pleasemailtokj@gmail.com>
2023-08-29 00:11:12 +0800

    add Update_image function

--- taipei-day-trip/MySQLTool.py
+++ taipei-day-trip/MySQLTool.py
@@ -8,7 +8,7 @@ db_config = {
     "database": "taipei_travel"
 }
 
-class MySQLtool(pooling.MySQLConnectionPool):
+class MySQLTool(pooling.MySQLConnectionPool):
     def __init__(self, **kargs):
         super().__init__(pool_name = "travel",
                          pool_size = 10,
@@ -18,6 +18,7 @@ class MySQLtool(pooling.MySQLConnectionPool):
         self.id = kargs.get('id')
         self.keyword = kargs.get("keyword")  
         self.attractions_to_update = kargs.get("attractions_to_update")
+        self.images_to_update = kargs.get("images_to_update")
     
     def Update_attraction(self):
         connection = self.get_connection()
@@ -45,16 +46,33 @@ class MySQLtool(pooling.MySQLConnectionPool):
         connection.commit()
         connection.close()
 
+    def Update_image(self):
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        update_string = (
+                        "insert into image(attraction_id, image)"
+                        "values(%s,%s)"
+                        )
+        
+        data_list = [(_["id"],_["image"]) for _ in self.images_to_update]
+                    
+        cursor.executemany(update_string, data_list)
+        connection.commit()
+        connection.close()
         
     def Search_attraction(self):      
         connection = self.get_connection()
         cursor = connection.cursor(dictionary=True)
 
         # create string for selecting data
-        select_string = "select * from image"
+        select_string = "select * from attraction"
         cursor.execute(select_string)
         result = cursor.fetchall()
 
         connection.close()
         return result
 
+# test = MySQLTool()
+# reuslt =test.Search_attraction()
+# print(reuslt)
--- taipei-day-trip/data/db.py
+++ taipei-day-trip/data/db.py
@@ -1,5 +1,5 @@
-from MySQLTool import MySQLtool
 import json
+from MySQLTool import MySQLTool
 
 with open('data/taipei-attractions.json', mode = "r") as file:
     result = file.read()
@@ -10,47 +10,52 @@ with open('data/taipei-attractions.json', mode = "r") as file:
 #   2-a. attraction : id, name, CAT, description, MRT, address, direction
 #   2-b. image : id, foreign_key (attractions's id), img_url
 
-# 2-a. organize data for table "attractions"
-attractions = []
-for _ in data:
-
-    # append orgaized data to "attraction" list
-    info = {
-        "name": _["name"],
-        "category": _["CAT"],
-        "description": _["description"],
-        "address": _["address"],
-        "transport": _["direction"],
-        "mrt": _["MRT"],
-        "lat": _["latitude"],
-        "lng": _["longitude"]
-    }
-    attractions.append(info)
-
-db = MySQLtool(attractions_to_update = attractions)
-db.Update_attraction()
-
-# # 2-b. organize data for table "image"
-# image = []
-# id = 0
+# ------ 2-a. organize data for table "attractions" -----
+# attractions = []
 # for _ in data:
-#     id += 1
-
-#     # delete img files which are not JPG or PNG
-#     file = _["file"]
-#     file_list = file.split("https://")
-
-#     img_url = []
-#     for file in file_list:
-#         if len(file) >= 4 and (file[-4:].lower() == ".jpg" or file[-4:].lower() == ".PNG"):
-#             url = "https://" + file
-#             img_url.append(url)
 
 #     # append orgaized data to "attraction" list
 #     info = {
-#         "id": id,
-#         "image": img_url
+#         "name": _["name"],
+#         "category": _["CAT"],
+#         "description": _["description"],
+#         "address": _["address"],
+#         "transport": _["direction"],
+#         "mrt": _["MRT"],
+#         "lat": _["latitude"],
+#         "lng": _["longitude"]
 #     }
-#     image.append(info)
+#     attractions.append(info)
+
+# db = MySQLtool(attractions_to_update = attractions)
+# db.Update_attraction()
+
+
+# ----- 2-b. organize data for table "image" -----
+images = []
+id = 0
+for _ in data:
+    id += 1
+
+    # delete img files which are not JPG or PNG
+    file = _["file"]
+    file_list = file.split("https://")
+
+    img_url = []
+    for file in file_list:
+        if len(file) >= 4 and (file[-4:].lower() == ".jpg" or file[-4:].lower() == ".PNG"):
+            url = "https://" + file
+            img_url.append(url)
+
+    # append orgaized data to "attraction" list
+    info = {
+        "id": id,
+        "image": img_url
+    }
+    images.append(info)
+
+db = MySQLTool(images_to_update = images)
+db.Update_image()
+
 
 

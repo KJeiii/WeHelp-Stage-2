e01e9433840e3446820cffce027ef9bc73c67bf6
KJeiii <pleasemailtokj@gmail.com>
2023-08-29 00:12:15 +0800

    add /api/attradtions route

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -7,14 +7,33 @@ app.config["TEMPLATES_AUTO_RELOAD"]=True
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+
 @app.route("/attraction/<id>")
 def attraction(id):
 	return render_template("attraction.html")
+
+
 @app.route("/booking")
 def booking():
 	return render_template("booking.html")
+
+
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
 
-app.run(host="0.0.0.0", port=3000)
+
+@app.route("/api/attractions")
+def attractions():
+	page  = request.args["page"]
+	keyword = request.args["keyword"]
+	response = {
+		"nextPage": int(page),
+		"keyword": keyword
+	}
+
+	return jsonify(response)
+
+
+app.run(host="0.0.0.0", port=3000, debug=True)

9278f0ad834e21d51523dc45ba40fbc4496c485b
KJeiii <pleasemailtokj@gmail.com>
2023-08-29 22:32:14 +0800

    re-organze 3 tables

--- taipei-day-trip/MySQLTool.py
+++ taipei-day-trip/MySQLTool.py
@@ -1,4 +1,5 @@
 from mysql.connector import connect, cursor, pooling
+from mysql.connector.conversion import MySQLConverter
 import os
 
 db_config = {
@@ -19,32 +20,56 @@ class MySQLTool(pooling.MySQLConnectionPool):
         self.keyword = kargs.get("keyword")  
         self.attractions_to_update = kargs.get("attractions_to_update")
         self.images_to_update = kargs.get("images_to_update")
+        self.mrts_to_update = kargs.get("mrts_to_update")
     
+
+    def Update_mrt(self):
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        update_string = (
+                        "insert into mrt (id, name, attraction_amount)"
+                        "values (%s, %s, %s)"
+                        )
+
+        data_list = [(_["id"],
+                     _["name"],
+                     _["attraction_amount"])
+                     for _ in self.mrts_to_update
+                    ]
+
+        cursor.executemany(update_string, data_list)
+        connection.commit()
+        connection.close()
+
+
     def Update_attraction(self):
         connection = self.get_connection()
         cursor = connection.cursor(dictionary=True)
 
         update_string = (
                         "insert into attraction("
-                        "name, category, description,"
-                        "address, transport, mrt, lat, lng)"
-                        "values(%s,%s,%s,%s,%s,%s,%s,%s)"
+                        "id, mrt_id, name, category, description,"
+                        "address, transport, lat, lng)"
+                        "values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
                         )
         
-        data_list = [(_["name"],
+        data_list = [(_["id"],
+                      _["mrt_id"],
+                      _["name"],
                       _["category"],
                       _["description"],
                       _["address"],
                       _["transport"],
-                      _["mrt"],
                       _["lat"],
                       _["lng"])
-                      for _ in self.attractions_to_update]
+                      for _ in self.attractions_to_update
+                      ]
                       
-
         cursor.executemany(update_string, data_list)
         connection.commit()
         connection.close()
+
 
     def Update_image(self):
         connection = self.get_connection()
@@ -55,12 +80,15 @@ class MySQLTool(pooling.MySQLConnectionPool):
                         "values(%s,%s)"
                         )
         
-        data_list = [(_["id"],_["image"]) for _ in self.images_to_update]
+        data_list = [(_["attraction_id"],
+                      _["image"]) 
+                      for _ in self.images_to_update]
                     
         cursor.executemany(update_string, data_list)
         connection.commit()
         connection.close()
         
+
     def Search_attraction(self):      
         connection = self.get_connection()
         cursor = connection.cursor(dictionary=True)
--- taipei-day-trip/data/db.py
+++ taipei-day-trip/data/db.py
@@ -6,39 +6,72 @@ with open('data/taipei-attractions.json', mode = "r") as file:
     data = json.loads(result)['result']['results']
 
 # 1. database called taipei_travel
-# 2. create two tables :
-#   2-a. attraction : id, name, CAT, description, MRT, address, direction
-#   2-b. image : id, foreign_key (attractions's id), img_url
+# 2. create 3 tables :
+#   2-a. mrt : id, name, attraction amount
+#   2-b. attraction : id, name, category, description, address, transport
+#   2-c. image : id, foreign_key (attractions's id), img_url
 
-# ------ 2-a. organize data for table "attractions" -----
-attractions = []
+# ------ 2-a. organize data for table "mrt" -----
+mrts_raw = [_["MRT"] for _ in data]
+mrts_set = set(mrts_raw)
+mrts_and_attraction = { _ : mrts_raw.count(_) for _ in mrts_set }
+
+mrts_dict = {} # create a dict for attraction table building foreign key
+n = 0
+for _ in mrts_set:
+    n += 1
+    mrts_dict[_] = {"id" : n,
+                    "attraction_amount" : mrts_and_attraction[_]
+                    }
+
+mrts_list = [] # create for insert data to MySQL
+for _ in mrts_dict:
+        mrts__list_element  = {"name" : _,
+                "id" : mrts_dict[_]["id"],
+                "attraction_amount" : mrts_dict[_]["attraction_amount"]
+             }
+        mrts_list.append(mrts__list_element)
+        
+# db = MySQLTool(mrts_to_update = mrts_list)
+# db.Update_mrt()
+
+
+# ------ 2-b. organize data for table "attractions" -----
+attractions_raw = [_["name"] for _ in data]
+attractions_id_dict = {}  # create a dict for image table building foreign key
+n = 0
+for _ in attractions_raw:
+    n += 1
+    attractions_id_dict[_] = n
+
+attractions_list = []
 for _ in data:
-
+    
     # append orgaized data to "attraction" list
     info = {
+        "id": attractions_id_dict[_["name"]],
+        "mrt_id": mrts_dict[_["MRT"]]["id"],
         "name": _["name"],
         "category": _["CAT"],
         "description": _["description"],
         "address": _["address"],
         "transport": _["direction"],
-        "mrt": _["MRT"],
         "lat": _["latitude"],
         "lng": _["longitude"]
     }
-    attractions.append(info)
+    attractions_list.append(info)
 
-# db = MySQLTool(attractions_to_update = attractions)
+# db = MySQLTool(attractions_to_update = attractions_list)
 # db.Update_attraction()
 
 
-# ----- 2-b. organize data for table "image" -----
-images = []
-id = 0
+# ----- 2-c. organize data for table "image" -----
+images_list = []
 for _ in data:
-    id += 1
     # delete img files which are not JPG or PNG
     file = _["file"]
     file_list = file.split("https://")
+
 
     for file in file_list:
         if len(file) >= 4 and (file[-4:].lower() == ".jpg" or file[-4:].lower() == ".png"):
@@ -46,13 +79,14 @@ for _ in data:
 
             # append orgaized data to "attraction" list
             info = {
-                "id": id,
+                "attraction_id": attractions_id_dict[_["name"]],
                 "image": url
             }
-            images.append(info)
+            images_list.append(info)
 
-# db = MySQLTool(images_to_update = images)
+# db = MySQLTool(images_to_update = images_list)
 # db.Update_image()
+
 
 
 

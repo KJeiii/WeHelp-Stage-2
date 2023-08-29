ebca38ff5a6b031009f0ceca07f9ae62b67968bd
KJeiii <pleasemailtokj@gmail.com>
2023-08-29 08:19:05 +0800

    complete part1-1

--- taipei-day-trip/data/db.py
+++ taipei-day-trip/data/db.py
@@ -11,23 +11,23 @@ with open('data/taipei-attractions.json', mode = "r") as file:
 #   2-b. image : id, foreign_key (attractions's id), img_url
 
 # ------ 2-a. organize data for table "attractions" -----
-# attractions = []
-# for _ in data:
+attractions = []
+for _ in data:
 
-#     # append orgaized data to "attraction" list
-#     info = {
-#         "name": _["name"],
-#         "category": _["CAT"],
-#         "description": _["description"],
-#         "address": _["address"],
-#         "transport": _["direction"],
-#         "mrt": _["MRT"],
-#         "lat": _["latitude"],
-#         "lng": _["longitude"]
-#     }
-#     attractions.append(info)
+    # append orgaized data to "attraction" list
+    info = {
+        "name": _["name"],
+        "category": _["CAT"],
+        "description": _["description"],
+        "address": _["address"],
+        "transport": _["direction"],
+        "mrt": _["MRT"],
+        "lat": _["latitude"],
+        "lng": _["longitude"]
+    }
+    attractions.append(info)
 
-# db = MySQLtool(attractions_to_update = attractions)
+# db = MySQLTool(attractions_to_update = attractions)
 # db.Update_attraction()
 
 
@@ -36,26 +36,23 @@ images = []
 id = 0
 for _ in data:
     id += 1
-
     # delete img files which are not JPG or PNG
     file = _["file"]
     file_list = file.split("https://")
 
-    img_url = []
     for file in file_list:
-        if len(file) >= 4 and (file[-4:].lower() == ".jpg" or file[-4:].lower() == ".PNG"):
+        if len(file) >= 4 and (file[-4:].lower() == ".jpg" or file[-4:].lower() == ".png"):
             url = "https://" + file
-            img_url.append(url)
 
-    # append orgaized data to "attraction" list
-    info = {
-        "id": id,
-        "image": img_url
-    }
-    images.append(info)
+            # append orgaized data to "attraction" list
+            info = {
+                "id": id,
+                "image": url
+            }
+            images.append(info)
 
-db = MySQLTool(images_to_update = images)
-db.Update_image()
+# db = MySQLTool(images_to_update = images)
+# db.Update_image()
 
 
 

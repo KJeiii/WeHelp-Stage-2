c9e604012b4c73867200211360b71a4f72e173c9
KJeiii <pleasemailtokj@gmail.com>
2023-09-02 16:58:12 +0800

    fix chinese wrong encoding : use app.json.esure_ascii = false

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -5,6 +5,7 @@ from MySQLTool import MySQLTool
 
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
+app.json.ensure_ascii = False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 
 # build MySQL connection

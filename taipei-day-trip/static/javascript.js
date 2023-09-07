e54109d38ef76d3629028ce37da8c8b7a76f83b3
KJeiii <pleasemailtokj@gmail.com>
2023-09-08 03:02:48 +0800

    complete part2-3&4

--- taipei-day-trip/static/javascript.js
+++ taipei-day-trip/static/javascript.js
@@ -1,12 +1,13 @@
-const url = "/api/attractions?page=0"
+const url = "/api/attractions"
 
-// build function for creating html element
+// ----- build function for creating html element -----
 const createElement = (TagName, className) => {
     const element = document.createElement(TagName);
     element.setAttribute("class", className);
     return element;
 };
 
+// ----- dynamically create homepage attraction element -----
 const createBottomDivElement = (element) => {
     //在container裡面有12個element，1個element有：
     // 1. image > background > name
@@ -40,33 +41,55 @@ const createBottomDivElement = (element) => {
 };
 
 // recored nextPage
-var nextPage = null;
+var nextPage = 0;
 
-const homePage = async() => {
-    // get data from api
-    let response = await fetch(url);
-    let data = await response.json();
-    let result = await data["data"];
+// build function for create new attraction element
+const loadPage = async(page, keyword) => {
+    if ( page !== null ) {
+        let params_string;
+        if (keyword === undefined) {
+            params_string = "?page=" + page;
+        }
+        else {
+            params_string = "?page=" + page + "&keyword=" + keyword;
+        }
+        
+        // get data from api
+        try {
+            let response = await fetch(url + params_string);
+            let data = await response.json();
+            let result = await data["data"];
 
-    // update nextPage
-    nextPage = data["nextPage"];
-
-    // create elements dynamically
-    let bottomDivContainer = createElement("div", "bottomDiv-container");
-
-    result.forEach(element => { 
-        let bottomDivContainerElement = createBottomDivElement(element);
-        bottomDivContainer.appendChild(bottomDivContainerElement);
-    });
-
-    let bottomDiv = document.querySelector(".bottomDiv");
-    bottomDiv.appendChild(bottomDivContainer);
+            if (!response.ok) {
+                console.log(response.status, typeof(response.status));
+                return response.status;
+            }
+    
+            // update nextPage
+            nextPage = data["nextPage"];
+    
+            // create elements dynamically
+            let bottomDivContainer = document.querySelector(".bottomDiv-container");
+    
+            result.forEach(element => { 
+                let bottomDivContainerElement = createBottomDivElement(element);
+                bottomDivContainer.appendChild(bottomDivContainerElement);
+            
+            return response.status;
+            })
+        }
+        catch (error){
+            console.log(error);
+            console.log(response.status);
+            return response.status;
+        }
+    }
 };
 
-homePage();
-setTimeout(() => {console.log(nextPage)},1000);
+loadPage(nextPage);
 
-// scroll page
+
+// ----- infinite scrolling -----
 options = {
     root: null,
     threshold: 1,
@@ -76,49 +99,80 @@ options = {
 const observer = new IntersectionObserver ((entries, observer) => {
     entries.forEach((entry, observer) => {
         if (entry.isIntersecting) {
+            setTimeout(loadPage(nextPage),500);
+
+            // change tracking element
+                // removeObserveEntity(entry);
+                // createBottomDivElement();
+            };
+        });
+    }, options);
+
+let footerDiv = document.querySelector(".footerDiv");
+observer.observe(footerDiv);
+    
       
-            // get api and create new elements
-            fetch("/api/attractions?page=" + nextPage)
-            .then(response => {
-                if (!response.ok) {
-                    console.log("Problem");
-                    return;
-                };
-                return response.json();
-            })
-            .then(data => {
-                // update nextPage
-                nextPage = data["nextPage"];
 
-                // create nextPage elements
-                result = data["data"]
-                let bottomDivContainer = document.querySelector(".bottomDiv-container");
-                result.forEach(element => {
-                    let bottomDivContainerElement = createBottomDivElement(element);
-                    bottomDivContainer.appendChild(bottomDivContainerElement);
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            })
 
-            console.log(entry.target.getBoundingClientRect().bottom);
 
-            // // change tracking element
-            // observer.unobserve(entry.target);
-            // let lenthOfElement = document.querySelectorAll(".bottomDiv-container-element").length;
-            // let elementForObserved = document.querySelectorAll(".bottomDiv-container-element")[lenthOfElement-1];
-            // observer.observe(elementForObserved);
-      
+// ----- build function for adding new entry for IntersectionObserver -----
+
+
+// const createObserveEntiy = () => {
+//     let lenthOfElement = document.querySelectorAll('.bottomDiv-container-element').length;
+//     let elementForObserved = document.querySelectorAll('.bottomDiv-container-element')[lenthOfElement-1];
+//     observer.observe(elementForObserved);
+// };
+
+// const removeObserveEntity = (entry) => {
+//     observer.unobserve(entry.target);
+// };
+
+
+
+// search attraciton
+const searchKeyword = () => {
+
+    let keyword = document.querySelector(".midDiv-container-searchBar-text").value;
+    if (loadPage(nextPage, keyword) === 200) {
+
+        // remove all child element in bottomDiv-container
+        let bottomDivContainer = document.querySelector(".bottomDiv-container");
+        while (bottomDivContainer.hasChildNodes()) {
+        bottomDivContainer.removeChild(bottomDivContainer.firstChild);
         };
-    });
-}, options);
 
-setTimeout(() => {
-    let lenthOfElement = document.querySelectorAll(".bottomDiv-container-element").length;
-    let elementForObserved = document.querySelectorAll(".bottomDiv-container-element")[lenthOfElement-1];
-    observer.observe(elementForObserved);
-    console.log(elementForObserved.getBoundingClientRect().bottom);
-}, 1000);
+        // instalize nextPage
+        nextPage = 0;
 
+        // update attraction
+        loadPage(nextPage, keyword);
+    }
+    else{
+        document.querySelector(".midDiv-container-searchBar-text").value = "無相符合景點";
+    }
+};
 
+const showMrt = async () => {
+    let url = "/api/mrts"
+
+    try{
+        let response = await fetch(url);
+        let data = await response.json();
+        let result = await data["data"];
+        console.log(result);
+
+        // dynamically create mrt 
+        let mrtDivMrtlistBarContent = document.querySelector(".mrtDiv-mrtlistBar-content");
+        result.forEach(element => {
+            let mrtDivMrtlistBarItem = createElement("div", "mrtDiv-mrtlistBar-item");
+            mrtDivMrtlistBarItem.textContent = element;
+            mrtDivMrtlistBarContent.appendChild(mrtDivMrtlistBarItem);
+        });
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+
+showMrt();

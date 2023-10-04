54f9cdb101ad6a0e38d2d313067e24e0477b0344
KJeiii <pleasemailtokj@gmail.com>
2023-10-04 23:32:41 +0800

    complete card prime setting

--- taipei-day-trip/static/booking.js
+++ taipei-day-trip/static/booking.js
@@ -1,27 +1,29 @@
-// booking function
-async function createItinerary () {
-    let itineraryInfo = {
-        "user_id": 6,
-        "attraction_id": 22,
-        "date": "2023-10-10",
-        "time": "afternoon",
-        "price": 2500
-    };
+// ----- booking function -----
+// async function createItinerary () {
+//     let itineraryInfo = {
+//         "user_id": 6,
+//         "attraction_id": 22,
+//         "date": "2023-10-10",
+//         "time": "afternoon",
+//         "price": 2500
+//     };
 
-    try{
-        let response = await fetch("/api/booking",{
-            method: "POST",
-            headers: {
-                "authorization": `Bearer ${window.localStorage.getItem("token")}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(itineraryInfo)
-            });
-    }
-    catch(error) {
-        console.log(error);
-    };        
-};
+//     try{
+//         let response = await fetch("/api/booking",{
+//             method: "POST",
+//             headers: {
+//                 "authorization": `Bearer ${window.localStorage.getItem("token")}`,
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(itineraryInfo)
+//             });
+//     }
+//     catch(error) {
+//         console.log(error);
+//     };        
+// };
+
+var itineraryInfo=null;
 
 function loadPage() {
     SignStatus().then((result) => {
@@ -42,7 +44,6 @@ function loadPage() {
                 //parse itinerary information from response body
                 itineraryInfo = data["data"];
 
-                console.log(itineraryInfo);
                 if (itineraryInfo !== null) {
                 // query all html elements need to be modified
                 let//
@@ -61,18 +62,18 @@ function loadPage() {
                 image.style.backgroundImage = `url(${itineraryInfo["attraction"]["image"]})`;
                 itineraryTitle.textContent = `台北一日遊：${itineraryInfo["attraction"]["name"]}`;
                 date.textContent = itineraryInfo["date"];
-                if (itineraryInfo["time"] === "beforenoon") {
-                    time.textContent = "早上9點到下午4點"
-                }
-                else{
-                    time.textContent = "下午4點到下午9點";
-                };
-                fee.textContent = `新台幣${itineraryInfo["price"]}元`;
-                address.textContent = itineraryInfo["attraction"]["address"];
-                contactName.value = result["data"]["name"];
-                contactEmail.value = result["data"]["email"];
-                totalPrice.textContent = `總價：新台幣${itineraryInfo["price"]}元`;
-                }
+                    if (itineraryInfo["time"] === "beforenoon") {
+                        time.textContent = "早上9點到下午4點"
+                    }
+                    else{
+                        time.textContent = "下午4點到下午9點";
+                    };
+                    fee.textContent = `新台幣${itineraryInfo["price"]}元`;
+                    address.textContent = itineraryInfo["attraction"]["address"];
+                    contactName.value = result["data"]["name"];
+                    contactEmail.value = result["data"]["email"];
+                    totalPrice.textContent = `總價：新台幣${itineraryInfo["price"]}元`;
+                    }
                 else{
                     let// 
                     itineraryDiv = document.querySelector(".itinerary"),
@@ -121,13 +122,15 @@ async function deleteItinerary() {
     });
 };
 
-// credit card tap pay SDK
+// ----- credit card tap pay SDK -----
+// set up SDK
 TPDirect.setupSDK(
     appID = 137169,
     appKey = "app_mb6V0dG1tbb6uz1CEVeP1uHHbleFJ8fRSqRnK64N2jL4PeLcx5BJwMicNA7i",
     serverType = "sandbox"
 );
 
+// set up html input
 let fields = {
     number: {
         element: "#card-number",
@@ -159,3 +162,51 @@ TPDirect.card.setup({
         }
     }
 });
+
+// POST payment information
+function checkBill() {
+    SignStatus()
+        .then(result => {
+            if (result["ok"] === true) {
+                let TPfieldsStatus = TPDirect.card.getTappayFieldsStatus();
+                if ( TPfieldsStatus.canGetPrime === true) {
+                    TPDirect.card.getPrime(result => {
+            
+                    // orgainze order info for database storage
+                    let orderInfo = {
+                        "prime": result.card.prime,
+                        "order": {
+                            "price": itineraryInfo["price"],
+                            "trip": {
+                                "attraction": {
+                                    "id": itineraryInfo["attraction"]["id"],
+                                    "name": itineraryInfo["attraction"]["name"],
+                                    "address": itineraryInfo["attraction"]["address"],
+                                    "image": itineraryInfo["attraction"]["image"]
+                                }},
+                            "date": itineraryInfo["date"],
+                            "time": itineraryInfo["time"]},
+                        "contact": {
+                            "name": result["name"],
+                            "email": result["email"],
+                            "phone": document.querySelector("input[name=contact-phone]").value}
+                        }
+
+                    // POST info
+                    fetch("", {
+                        method: "POST",
+                        headers: {
+                            "Authorization": `Bearer ${window.localStorage.getItem("token")}`,
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(orderInfo)
+                    })
+                    .then(res => {console.log(res)})
+                    .catch(err => {console.log(err)})
+                    });
+                }
+            }
+        })
+        .catch(error => {console.log(error)})
+};
+

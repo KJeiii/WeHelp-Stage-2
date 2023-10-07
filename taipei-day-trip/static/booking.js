edf8d3ce565cfa9f15aab1a002c3924bd4c575ee
KJeiii <pleasemailtokj@gmail.com>
2023-10-07 23:40:30 +0800

    clean code

--- taipei-day-trip/static/booking.js
+++ taipei-day-trip/static/booking.js
@@ -1,28 +1,3 @@
-// ----- booking function -----
-// async function createItinerary () {
-//     let itineraryInfo = {
-//         "user_id": 6,
-//         "attraction_id": 22,
-//         "date": "2023-10-10",
-//         "time": "afternoon",
-//         "price": 2500
-//     };
-
-//     try{
-//         let response = await fetch("/api/booking",{
-//             method: "POST",
-//             headers: {
-//                 "authorization": `Bearer ${window.localStorage.getItem("token")}`,
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify(itineraryInfo)
-//             });
-//     }
-//     catch(error) {
-//         console.log(error);
-//     };        
-// };
-
 var itineraryInfo=null;
 
 function loadPage() {

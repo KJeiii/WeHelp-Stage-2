ef4843b0e4a87b6770c98fa6ed57c0fef1e80002
KJeiii <pleasemailtokj@gmail.com>
2023-10-11 09:45:02 +0800

    move import blueprint instances to app.py from TaipeiTravel __init__

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -1,4 +1,15 @@
 from TaipeiTravel import app
+from TaipeiTravel.attraction import attraction_bp
+from TaipeiTravel.itinerary import itinerary_bp
+from TaipeiTravel.member import member_bp
+from TaipeiTravel.payment import payment_bp
+from TaipeiTravel.thankyou import thankyou_bp
+
+app.register_blueprint(attraction_bp)
+app.register_blueprint(itinerary_bp)
+app.register_blueprint(member_bp)
+app.register_blueprint(payment_bp)
+app.register_blueprint(thankyou_bp)
 
 app.run(host="0.0.0.0", port=3000, debug=True)
 
